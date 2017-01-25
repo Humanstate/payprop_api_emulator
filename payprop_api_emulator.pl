09ce9e0ffca9361eb9fbd47bfbc7b3831b446dfd
Lee Johnson <lee@givengain.ch>
2017-01-25 17:05:15 +0100

    helper can be set without having to pass anything
    we can override helpers after the register step, so passing a code
    ref in the register process isn't necessary

--- payprop_api_emulator.pl
+++ payprop_api_emulator.pl
@@ -59,16 +59,19 @@ $api->options( '/*whatever' => sub {
 plugin OpenAPI => {
 	route => $api,
 	url   => "https://za.payprop.com/api/docs/api_spec.yaml",
-	not_implemented => sub {
-		my ( $c,$spec ) = @_;
-
-		if (my ($response) = grep { /^2/ } sort keys(%{$spec->{'responses'}})) {
-			my $schema = $spec->{'responses'}{$response}{schema};
-			return JSON::Schema::ToJSON->new->json_schema_to_json( schema => $schema );
-		}
-
-		return {errors => [{message => 'Not implemented.', path => '/'}], status => 501};
-	}
 };
+
+app->helper( 'openapi.not_implemented' => sub {
+	my ( $c ) = @_;
+
+	my $spec = $c->openapi->spec;
+	if (my ($response) = grep { /^2/ } sort keys(%{$spec->{'responses'}})) {
+		my $schema = $spec->{'responses'}{$response}{schema};
+		return JSON::Schema::ToJSON->new->json_schema_to_json( schema => $schema );
+	}
+
+	return {errors => [{message => 'Not implemented.', path => '/'}], status => 501};
+});
+
 
 app->start;
